2b9dc0aede780632baa217f870b7486614ad8e11
Connor Lehmann <lehmannjconnor@gmail.com>
2026-07-21 20:50:19 -0400

    testing again

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
-robot_x = 50
-robot_y = 50
-robot = pygame.draw.rect(screen, (0, 255, 0), (400, 300, robot_x, robot_y))
+robot_x = 400
+robot_y = 300
+robot = pygame.draw.rect(screen, (0, 255, 0), (robot_x, robot_y, 10, 10))
 pygame.draw.rect(screen, (255, 0, 0), (400, 300, 50, 50))
 pygame.display.update()
 
@@ -22,8 +22,8 @@ while True:
             robot_x -= 5
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
             robot_x += 5
-        
-        robot = pygame.draw.rect(screen, (0, 255, 0), (400, 300, robot_x, robot_y))
+            
+            robot = pygame.draw.rect(screen, (0, 255, 0), (robot_x, robot_y, 10, 10))
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
