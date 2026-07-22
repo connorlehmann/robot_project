b8fd5764d847117c500cfb5df1fc466ca76bc4b0
Connor Lehmann <lehmannjconnor@gmail.com>
2026-07-21 20:43:27 -0400

    testing if these hypothetical controls work

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
-robot_x = 0
-robot_y = 0
+robot_x = 50
+robot_y = 50
+robot = pygame.draw.rect(screen, (0, 255, 0), (400, 300, robot_x, robot_y))
+pygame.draw.rect(screen, (255, 0, 0), (400, 300, 50, 50))
 
 while True:
-    
+
     for event in pygame.event.get():
+
         if pygame.key.get_pressed()[pygame.K_UP]:
             robot_y += 5
         if pygame.key.get_pressed()[pygame.K_DOWN]:
@@ -19,6 +22,7 @@ while True:
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
             robot_x += 5
         
+        robot = pygame.draw.rect(screen, (0, 255, 0), (400, 300, robot_x, robot_y))
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
