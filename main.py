b9182965bc87ab3593b0f97c93dda4be1696697a
Connor Lehmann <lehmannjconnor@gmail.com>
2026-07-23 11:18:28 -0400

    control refinement

--- main.py
+++ main.py
@@ -21,25 +21,19 @@ while True:
     pygame.draw.rect(screen, (0, 0, 255), (475, 375, 10, 10))
 
     for event in pygame.event.get():
-
-        if pygame.key.get_pressed()[pygame.K_UP]:
-            robot1.speed_forward()
-        if pygame.key.get_pressed()[pygame.K_DOWN]:
-            robot1.speed_backward()
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            robot1.turn_left()
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            robot1.turn_right()
-        
-        
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
             
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        
+    if pygame.key.get_pressed()[pygame.K_UP]:
+        robot1.speed_forward()
+    if pygame.key.get_pressed()[pygame.K_DOWN]:
+        robot1.speed_backward()
+    if pygame.key.get_pressed()[pygame.K_LEFT]:
+        robot1.turn_left()
+    if pygame.key.get_pressed()[pygame.K_RIGHT]:
+        robot1.turn_right()   
+             
     if  475 < robot1.pos_x < 485 and 375 < robot1.pos_y < 385:
         print("Robot has reached the target!")
         pygame.quit()
