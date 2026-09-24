d5a81743f23b1eb6237a13a3b4bf827436a61a7b
Connor Lehmann <lehmannjconnor@gmail.com>
2026-09-23 21:38:36 -0400

    upping the step amount

--- check_env.py
+++ check_env.py
@@ -4,7 +4,7 @@ env = RobotEnv()
 obs, info = env.reset()
 print("Initial obs:", obs)
 
-for i in range(30):
+for i in range(200):
     obs, reward, terminated, truncated, info = env.step(1)  # forward
     print(f"step={i}  obs={obs}  terminated={terminated}")
     if terminated:
