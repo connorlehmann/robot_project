67ba93f66b400ea66bbbe5eecaa0aee1ba65541d
Connor Lehmann <lehmannjconnor@gmail.com>
2026-09-23 21:37:08 -0400

    testing on actual check enviroment

---
+++ check_env.py
@@ -0,0 +1,12 @@
+from environment import RobotEnv
+
+env = RobotEnv()
+obs, info = env.reset()
+print("Initial obs:", obs)
+
+for i in range(30):
+    obs, reward, terminated, truncated, info = env.step(1)  # forward
+    print(f"step={i}  obs={obs}  terminated={terminated}")
+    if terminated:
+        print("Hit something, stopping")
+        break
