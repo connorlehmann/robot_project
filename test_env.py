f4d24f5f4b683e2cfd9caad942a2230de2897c55
Connor Lehmann <lehmannjconnor@gmail.com>
2026-09-23 21:43:12 -0400

    checking again

--- test_env.py
+++ test_env.py
@@ -1,18 +1,3 @@
-from environment import RobotEnv
-
-env = RobotEnv()
-obs, info = env.reset()
-print("Initial obs:", obs)
-
-actions_to_try = [1, 1, 1, 3, 3, 0]  # forward, forward, forward, turn_right, turn_right, stop
-
-for action in actions_to_try:
-    obs, reward, terminated, truncated, info = env.step(action)
-    print(f"action={action}  obs={obs}  terminated={terminated}")
-    if terminated:
-        print("Episode ended, resetting")
-        obs, info = env.reset()
-
 from stable_baselines3.common.env_checker import check_env
 from environment import RobotEnv
 
