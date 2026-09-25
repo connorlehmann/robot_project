58c9e0cb591c52160c080b49453d8e34cda5237a
Connor Lehmann <lehmannjconnor@gmail.com>
2026-09-25 14:41:57 -0400

    training for more steps

--- train.py
+++ train.py
@@ -6,6 +6,6 @@ from environment import RobotEnv
 env = RobotEnv()
 
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./ppo_robot_tensorboard/")
-model.learn(total_timesteps=10000)
+model.learn(total_timesteps=200000)
 
 model.save("robot_policy")
