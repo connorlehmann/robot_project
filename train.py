6a4bc39e81ee53c59eb5b3b5977dbdad5201177e
Connor Lehmann <lehmannjconnor@gmail.com>
2026-09-24 22:45:12 -0400

    creating the training for the robot

---
+++ train.py
@@ -0,0 +1,11 @@
+from stable_baselines3 import PPO
+
+from stable_baselines3 import PPO
+from environment import RobotEnv
+
+env = RobotEnv()
+
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./ppo_robot_tensorboard/")
+model.learn(total_timesteps=10000)
+
+model.save("robot_policy")
